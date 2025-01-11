10e6c5e96943cb9350d05814d4bd8ad40225c784
Aditya <adityagangwar516@gmail.com>
2025-01-11 14:43:55 +0530

    video

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -31,12 +31,16 @@ export default function HeroCarousel() {
         }}
       />
        <div style={{ height: "27rem", width: "100%" }}>
-        <iframe 
-          src="https://www.tiktok.com/embed/7143951736766647579" 
-          style={{ height: "100%", width: "100%", border: "none" }}
-          allowFullScreen
-        ></iframe>
+       <video 
+  src="https://media-hosting.imagekit.io//8164342df7c94e27/VID-20250111-WA0008.mp4?Expires=1831194290&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=rGlLffKxMxh0iw4PISa0NnTJCyd3j8w~lh0URdWqhDzZd-S8VTxM31I~AIbsvDZt82MR7hIkGsu2JLgL8PnQ9zAGaL2qkzXMmKnjKk6jCt5~ufWucEP1G0~CsKInjqD43zxx3rPypvIgoCupQXIvuglxp4HvpCq9IYPTVo5BlwnLe6BCO7~gWnUSEaJd5D3evZPDMmMKmSD2LczZXJJkdQ4OhP76JVU0Gm~TdQsYaAINn9TsTuTVNi1TNKVoE9wuyDrvRJ48E9eqYalNVtEPaEm2YjSE1mYGW94w~09FUrbR7dgQ-7YKKHI2ACekDFoKlMBp8H5V~6wHiutlcyk57g__"
+  style={{ height: "100%", width: "100%", border: "none" }}
+  autoPlay
+  muted
+  playsInline
+></video>
+
       </div>
     </div>
   );
 }
+
